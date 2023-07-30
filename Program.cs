f0923c76c53651d187698a787a93b55b4e693398
Mishka <lp-fan1992@mail.ru>
2023-07-30 15:44:24 +0300

    Finish

--- Program.cs
+++ Program.cs
@@ -100,4 +100,64 @@ int column = Convert.ToInt32(Console.ReadLine());
 CheckElement(row, column, myArray);
 */
 
+/*Задача 52. Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.
+*/
+
+int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+    for(int i=0; i<rows; i++)
+    
+        for(int j=0; j<columns;j++)
+        
+            array[i,j] = new Random().Next(minValue, maxValue+1);
+        
+    return array;
+}
+void Show2dArray(int [,] array)
+{
+    for(int i = 0; i<array.GetLength(0); i++)
+    {
+        for(int j = 0; j<array.GetLength(1); j++)
+            Console.Write(array[i,j]+ " ");
+
+        Console.WriteLine();
+    }
+}
+
+double[] Average(int[,] array)
+{
+    double[] averarray = new double[array.GetLength(1)];
+    
+    for(int j = 0; j<array.GetLength(1); j++)
+    {   double result1  = 0;
+        for(int i = 0; i<array.GetLength(0); i++)
+        {
+            result1=result1+array[i,j];
+        }
+        double avercol=result1/(double)array.GetLength(0);
+        averarray[j] = avercol;
+        Console.WriteLine("Column sum " + (j+1)+" = " +result1);
+        Console.WriteLine("Average element is "+(j+1)+" = " +avercol);
+    }
+    return averarray;
+}
+
+Console.Write("Input a number of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a number of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("input a minimum possible: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a maximum possible: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] MyArray = CreateRandom2dArray(rows, columns, minValue, maxValue);
+
+Show2dArray(MyArray);
+Average(MyArray);
+
+
+
 
